21d9b4abe89c4108447754a05b9907307e313a34
Louis <louis20019028@gmail.com>
2024-12-26 17:31:12 +0800

    Initial Commit

---
+++ Cat/CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApp.swift
+//  Cat
+//
+//  Created by Louis Chang on 2024/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatTests/CatTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatTests.swift
+//  CatTests
+//
+//  Created by Louis Chang on 2024/12/26.
+//
+
+import Testing
+@testable import Cat
+
+struct CatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
